09901bfe26c75fd7d6de892a650d7b0fbf06add7
Ashwin <ashwin.anil@gmail.com>
2022-12-17 21:52:45 +0530

    Added: Hardcoded event Data

---
+++ src/assets/eventData.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        "title": "24 Hour Hackathon",
+        "theme": ["blockchain", "Social", "AI/ML", "HealthTech"],
+        "keywords": ["Online", "Open"],
+        "participantsNo": 500,
+        "imgUrl": "https://plus.unsplash.com/premium_photo-1661684579181-ca26952b81c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "title": "24 Hour Hackathon",
+        "theme": ["blockchain", "Social", "AI/ML", "HealthTech"],
+        "keywords": ["Online", "Open"],
+        "participantsNo": 500,
+        "imgUrl": "https://plus.unsplash.com/premium_photo-1661684579181-ca26952b81c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "title": "24 Hour Hackathon",
+        "theme": ["blockchain", "Social", "AI/ML", "HealthTech"],
+        "keywords": ["Offline", "Open", "Girls Only"],
+        "participantsNo": 500,
+        "imgUrl": "https://plus.unsplash.com/premium_photo-1661684579181-ca26952b81c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "title": "24 Hour Hackathon",
+        "theme": ["blockchain", "Social", "AI/ML", "HealthTech"],
+        "keywords": ["Offline", "Open"],
+        "participantsNo": 500,
+        "imgUrl": "https://plus.unsplash.com/premium_photo-1661684579181-ca26952b81c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "title": "24 Hour Hackathon",
+        "theme": ["blockchain", "Social", "AI/ML", "HealthTech"],
+        "keywords": ["Online", "Open"],
+        "participantsNo": 500,
+        "imgUrl": "https://plus.unsplash.com/premium_photo-1661684579181-ca26952b81c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "title": "24 Hour Hackathon",
+        "theme": ["blockchain", "Social", "AI/ML", "HealthTech"],
+        "keywords": ["Online", "Open"],
+        "participantsNo": 500,
+        "imgUrl": "https://plus.unsplash.com/premium_photo-1661684579181-ca26952b81c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+]
