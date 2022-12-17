60bc9304149e91fb86e9d0bd22f7f1e97f8fed66
Bennett B Madavana <34763279+bennett1412@users.noreply.github.com>
2022-12-18 00:00:56 +0530

    Updated signup page

--- src/pages/Register/components/signup.jsx
+++ src/pages/Register/components/signup.jsx
@@ -1,92 +1,73 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-function SignUp(){
-    return ( 
+function SignUp() {
+    return (
         <div>
             <form>
-             <br/>
-            <br/>
-            <h5>Type</h5>
-            <input 
-                type="radio"
-                value="Sponsor"
-                name="role"
+                <h5>Type</h5>
+                <input
+                    type="radio"
+                    value="Sponsor"
+                    name="role"
+                />
+                <label for="html">Sponsor</label>&emsp;
+                <input
+                    type="radio"
+                    value="Host"
+                    name="role"
+                />
+                <label for="html">Host</label>&emsp;
+                <input
+                    type="radio"
+                    value="User"
+                    name="role"
+                />
+                <label for="html">User</label>
 
-                            />
-            <label for="html">Sponsor</label>&emsp;
-            <input 
-                type="radio"
-                value="Host"
-                name="role"
+                <input
+                    type="text"
+                    placeholder="Enter your Organization"
+                />
 
-            />
-            <label for="html">Host</label>&emsp;
-            <input 
-                type="radio"
-                value="User"
-                name="role"
+                <input
+                    type="text"
+                    placeholder="Enter your location"
 
-            />
-            <label for="html">User</label>
-            <br/>
-            <br/>
-        
-            <input 
-                type="text"
-                placeholder="Enter your Organization"
+                />
 
-            />
-            <br/>
-            <br/>
-            <input 
-                type="text"
-                placeholder="Enter your location"
-               
-            />
-            <br/>
-            <br/>
-            <input 
-                type="text"
-                placeholder="Enter your dept"
-               
-            />
-            <br/>
-            <br/>
-            <input 
-                type="text"
-                placeholder="Enter your sem"
-               
-            />
-            <br/>
-            <br/>
-            <input 
-                type="text"
-                placeholder="Enter your company name"
-               
-            />
-            <br/>
-            <br/>
-            <input 
-                type="text"
-                placeholder="Enter your Contact"
-               
-            />
-            <br/>
-            <br/>
-            <textarea placeholder='Enter your company description'>
-                
-            </textarea>
-            <br/>
-            <br/>
-            
-            <Button>
-               SignUp
-            </Button>
+                <input
+                    type="text"
+                    placeholder="Enter your dept"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Enter your sem"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Enter your company name"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Enter your Contact"
+                />
+
+                <textarea placeholder='Enter your company description'>
+
+                </textarea>
+
+
+                <Button>
+                    SignUp
+                </Button>
             </form>
         </div>
 
-     );
+    );
 }
- 
+
 export default SignUp;
