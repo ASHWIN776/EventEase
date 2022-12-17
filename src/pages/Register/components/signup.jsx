292cc63db39fe494aff199a395a4f2560f50063d
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-12-17 17:33:17 +0530

    Added: Button added in register screen

---
+++ src/pages/Register/components/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUp(){
+    return ( 
+        <div className="App">
+           ddddd
+        </div>
+
+     );
+}
+ 
+export default SignUp;
--- src/pages/Register/register.jsx
+++ src/pages/Register/register.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavScrollExample1 from './components/navbarRegister'
+import SignUp from './components/signup'
 
 
 const Register = () => {
     return (
         <div>
             <NavScrollExample1/>
+            <SignUp/>
         </div>
     )
 }
