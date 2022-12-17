a6fef3e6407f050d25295fb251cd0581d43d80ee
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2022-12-17 23:22:56 +0530

    Passed: Event Title to viewMoreEvent screen

--- src/pages/User/components/Event.jsx
+++ src/pages/User/components/Event.jsx
@@ -38,7 +38,7 @@ export default function Event(props)
                     {themeArr}
                 </div>
                 <Card.Text>Venue: <span>{props.event.venue}</span></Card.Text>
-                <Button variant="primary" onClick={() => {navigate('/ViewMore')}}>View More</Button>
+                <Button variant="primary" onClick={() => {navigate('/ViewMore',{state: {title: props.event.title,}})}}>View More</Button>
             </Card.Body>
         </Card>
     )
