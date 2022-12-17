9276744634cbed45fa042341c3dd78e6defeda64
Bennett B Madavana <34763279+bennett1412@users.noreply.github.com>
2022-12-17 16:22:54 +0530

    Added firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Auth from './pages/Auth/Auth'
 import Home from './pages/Home/Home'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
       </Routes>
     </div>
   )
